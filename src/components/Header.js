5cd88bbff0a0ad733d5df57124a49baaf2dc82dc
Thomas Greenhalgh <thomasgreenhalgh@gmail.com>
2018-07-02 11:57:57 -0700

    adds routing

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,40 @@
 import React from 'react';
 import { Button } from 'reactstrap';
 
-const Header = () => (
-  <div className="row">
-    <div className="col-sm-4">
-      <Button color="primary" style={{ marginLeft: '10%' }}>
-        IncomeGraph
-      </Button>
+const Header = props => {
+  const homeClick = () => {
+    props.history.push('/');
+  };
+
+  const formClick = () => {
+    props.history.push('/form');
+  };
+
+  const graphClick = () => {
+    props.history.push('/graph');
+  };
+
+  return (
+    <div className="row">
+      <div className="col-sm-4">
+        <Button
+          color="primary"
+          style={{ marginLeft: '10%' }}
+          onClick={homeClick}
+        >
+          IncomeGraph
+        </Button>
+      </div>
+      <div className="col-sm-8">
+        <Button color="info" onClick={formClick}>
+          Form
+        </Button>
+        <Button color="info" onClick={graphClick}>
+          Graph
+        </Button>
+      </div>
     </div>
-    <div className="col-sm-8">
-      <Button color="info">Form</Button>
-      <Button color="info">Graph</Button>
-    </div>
-  </div>
-);
+  );
+};
 
 export default Header;
