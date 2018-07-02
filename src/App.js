55904fd50cfe26823e44097078b5a924c08ff43f
Thomas Greenhalgh <thomasgreenhalgh@gmail.com>
2018-07-02 14:38:20 -0700

    makes App functional

--- src/App.js
+++ src/App.js
@@ -1,44 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Route } from 'react-router-dom';
 import Header from './components/Header';
 import InputForm from './components/InputForm';
 import './App.css';
 
-class App extends Component {
-  constructor() {
-    super();
-    /* prettier-ignore */
-    this.state = {
-      userInfo: {},
-    };
-  }
-
-  updateData = data => {
-    this.setState({ data });
-  };
-
-  updateUserInfo = userInfo => {
-    this.setState({ userInfo });
-  };
-
-  render() {
-    return (
-      <div className="App">
-        <Route path="/" component={Header} />
-        <Route
-          exact
-          path="/form"
-          render={props => (
-            <InputForm
-              {...props}
-              handleUserUpdate={this.updateUserInfo}
-              data={JSON.stringify(this.state.userInfo)}
-            />
-          )}
-        />
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div className="App">
+    <Route path="/" component={Header} />
+    <Route exact path="/form" render={props => <InputForm {...props} />} />
+  </div>
+);
 
 export default App;
