f004fea479518b2622c9bb364f59f547dcec8d8a
Mark Hermansen <markwilliams.aus@gmail.com>
2018-07-02 20:27:04 -0500

    cleaned it up a little bit

--- src/components/Graph.js
+++ src/components/Graph.js
@@ -11,43 +11,43 @@ class Graph extends React.Component {
           {
             label: '',
             fill: false,
-            lineTension: 0.1,
-            backgroundColor: 'rgba(75,192,192,0.4)',
-            borderColor: 'rgba(75,192,192,1)',
-            borderCapStyle: 'butt',
+            lineTension: 0,
+            backgroundColor: '',
+            borderColor: '',
+            borderCapStyle: '',
             borderDash: [],
-            borderDashOffset: 0.0,
-            borderJoinStyle: 'miter',
-            pointBorderColor: 'rgba(75,192,192,1)',
-            pointBackgroundColor: '#fff',
-            pointBorderWidth: 1,
-            pointHoverRadius: 5,
-            pointHoverBackgroundColor: 'rgba(75,192,192,1)',
-            pointHoverBorderColor: 'rgba(220,220,220,1)',
-            pointHoverBorderWidth: 2,
-            pointRadius: 1,
-            pointHitRadius: 10,
+            borderDashOffset: 0,
+            borderJoinStyle: '',
+            pointBorderColor: '',
+            pointBackgroundColor: '',
+            pointBorderWidth: 0,
+            pointHoverRadius: 0,
+            pointHoverBackgroundColor: '',
+            pointHoverBorderColor: '',
+            pointHoverBorderWidth: 0,
+            pointRadius: 0,
+            pointHitRadius: 0,
             data: [],
           },
           {
             label: '',
             fill: false,
-            lineTension: 0.1,
-            backgroundColor: 'rgba(75,192,192,0.4)',
-            borderColor: 'rgba(75,192,192,1)',
-            borderCapStyle: 'butt',
+            lineTension: 0,
+            backgroundColor: '',
+            borderColor: '',
+            borderCapStyle: '',
             borderDash: [],
-            borderDashOffset: 0.0,
-            borderJoinStyle: 'miter',
-            pointBorderColor: 'rgba(75,192,192,1)',
-            pointBackgroundColor: '#fff',
-            pointBorderWidth: 1,
-            pointHoverRadius: 5,
-            pointHoverBackgroundColor: 'rgba(75,192,192,1)',
-            pointHoverBorderColor: 'rgba(220,220,220,1)',
-            pointHoverBorderWidth: 2,
-            pointRadius: 1,
-            pointHitRadius: 10,
+            borderDashOffset: 0,
+            borderJoinStyle: '',
+            pointBorderColor: '',
+            pointBackgroundColor: '',
+            pointBorderWidth: 0,
+            pointHoverRadius: 0,
+            pointHoverBackgroundColor: '',
+            pointHoverBorderColor: '',
+            pointHoverBorderWidth: 0,
+            pointRadius: 0,
+            pointHitRadius: 0,
             data: [],
           }
         ]
@@ -112,13 +112,16 @@ class Graph extends React.Component {
   render() {
     return (
       <div className="chart">
-        <Line data={this.state.chartData} options={{
+        <Line data={this.state.chartData}
+        options={{
           title: {
             display: true,
             text: 'Current and New Income Projections',
             fontSize: 25,
           }
-        }} />
+        }} 
+        datasetKeyProvider={() => Math.random()}
+        />
       </div>
     )
   }
@@ -126,60 +129,6 @@ class Graph extends React.Component {
 }
 
 export default Graph;
-
-// export default React.createClass({
-//   displayName: 'IncomeGraph',
-//   render() {
-//     return (
-//       <div>
-//         <Line data={data} />
-//       </div>
-//     )
-//   }
-// })
-
-
-// const data = props => {
-//   console.log('data', props.data);
-//   props.data is data for the graph
- 
-//   dynamicLabels = (data) => {
-//     let label = [];
-//     let year = 2018;
-//    for (let i = 0; i < data.length; i++) {
-//      label.push(year);
-//      year++;
-//    }
-//    return label;
-//   }
- 
-//   let data = {
-//     datasets: [
-//       {
-//         label: 'Current Income',
-//         fill: false,
-//         lineTension: 0.1,
-//         backgroundColor: 'rgba(75,192,192,0.4)',
-//         borderColor: 'rgba(75,192,192,1)',
-//         borderCapStyle: 'butt',
-//         borderDash: [],
-//         borderDashOffset: 0.0,
-//         borderJoinStyle: 'miter',
-//         pointBorderColor: 'rgba(75,192,192,1)',
-//         pointBackgroundColor: '#fff',
-//         pointBorderWidth: 1,
-//         pointHoverRadius: 5,
-//         pointHoverBackgroundColor: 'rgba(75,192,192,1)',
-//         pointHoverBorderColor: 'rgba(220,220,220,1)',
-//         pointHoverBorderWidth: 2,
-//         pointRadius: 1,
-//         pointHitRadius: 10,
-//         data: props.data,
-//       }
-//     ]
-//   }; 
-// };
-
 
 
 
